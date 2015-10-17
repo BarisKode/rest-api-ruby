b83d8fec8b90c334c4fb7c7c3f32903a5b1c43c7
rohmad-st <rochmad.26@gmail.com>
2015-10-18 00:26:30 +0700

    Initial commit

--- app/api/employee/data.rb
+++ app/api/employee/data.rb
@@ -3,10 +3,50 @@ module Employee
 
 
     resource :employee_data do
+      ## Show list of employee
       desc "List all Employee"
 
       get do
         EmpDatum.all
+      end
+
+      ## This takes care of parameter validation
+      params do
+        requires :name, type: String
+        requires :address, type: String
+        requires :age, type: Integer
+      end
+
+      ## This takes care of creating employee
+      post do
+        EmpDatum.create!({
+                             name:    params[:name],
+                             address: params[:address],
+                             age:     params[:age]
+                         })
+      end
+
+      ## This takes care of updating employee
+      desc "update an employee address"
+      params do
+        requires :id, type: String
+        requires :address, type: String
+      end
+
+      put ':id' do
+        EmpDatum.find(params[:id]).update({
+                                              address: params[:address]
+                                          })
+      end
+
+      ## This takes care of destroy employee
+      desc "delete an employee"
+      params do
+        requires :id, type: String
+      end
+
+      delete ':id' do
+        EmpDatum.find(params[:id]).destroy!
       end
 
     end
