78beb49d247be7c6bc15b5206a95f8942bb81c25
rohmad-st <rochmad.26@gmail.com>
2015-10-18 00:48:44 +0700

    Initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017162624) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.text     "content",     limit: 65535
-    t.integer  "category_id", limit: 4
-    t.integer  "author_id",   limit: 4
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-  end
-
-  create_table "emp_data", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "address",    limit: 255
-    t.integer  "age",        limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
+ActiveRecord::Schema.define(version: 20151017154605) do
 
 end
